8ea6110dcc84c2d2aee69a7c6b67903203b38c90
SaSaaaaaaaaa <shrxnz2332@gmail.com>
2024-09-27 14:28:33 +0700

    percobaan 1

---
+++ PemilihanBilangan7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; // import scanner
+
+public class PemilihanBilangan7{
+    public static void main(String[] args) { // fungsi main()
+        Scanner scan = new Scanner(System.in); // Deklarasi scanner
+        int angka; // initialisasi variabel angka dengan tipe data integer
+
+        System.out.print("Masukkan sebuah angka : "); // untuk menginput angka 
+        angka = scan.nextInt(); // fungsi input angka
+
+        // menggunakan fungsi percabangan if else
+        if (angka % 2 == 0){  // persyaratan pertama if, modulus angka apabila dibagi dengan 2 sama dengan 0
+            System.out.println("Angka " + angka + " termasuk bilangan genap"); // apabila benar, maka angka termasuk bilangan genap
+        } else { // apabila persyaratan if tidak terpenuhi, maka lainnya akan menghasilkan output seperti berikut
+            System.out.println("Angka " + angka + " termasuk bilangan ganjil");  // apabila benar, maka angka termasuk bilangan ganjil
+        }
+    }
+}
